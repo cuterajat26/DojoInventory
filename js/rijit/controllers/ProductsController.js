3667efd392d8b1e179f08ca537f2bc455ca29037
Rajat Talwar <cuterajat26@gmail.com>
2013-05-12 20:10:41 +0530

    crude File handling done,code needs a lot of improvement and modularity

--- js/rijit/controllers/ProductsController.js
+++ js/rijit/controllers/ProductsController.js
@@ -10,17 +10,18 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
                          
                            baseClass: "rijitController",
                           templateString: template,
-                      
-  
+                          Product:new Product(),
+                          formData:null,
                       postCreate: function(){
                                   this.inherited(arguments);
+
                                   eval("this."+this.action+"();");
                                   },
                       add:function(){
                         var form = new FormHelper();
                          var formDom = form.createForm("Product");
                         domConstruct.place(formDom,this.body,"last");
-                        form.input("Image",{title:"Add Image",type:"file"});
+                        form.input("image",{title:"Add Image",type:"file"});
 
                         form.input("name",{width:"50em"});
 
@@ -28,7 +29,7 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
 
                         form.input("quantity");
                         form.input("price");
-                        form.end("Submit it",lang.hitch(this,"_addedSuccessfully"));
+                        form.end("Submit it",lang.hitch(this,"_addedSubmitted"));
 
                       },
                       list:function(){
@@ -38,9 +39,85 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
                       _onList:function(item){
                          
                       },
-                      _addedSuccessfully:function(){
-                        alert("heyaa");
+                      _fileErrorHandler:function(e){
+                         var msg = '';
+
+                        switch (e.code) {
+                          case FileError.QUOTA_EXCEEDED_ERR:
+                            msg = 'QUOTA_EXCEEDED_ERR';
+                            break;
+                          case FileError.NOT_FOUND_ERR:
+                            msg = 'NOT_FOUND_ERR';
+                            break;
+                          case FileError.SECURITY_ERR:
+                            msg = 'SECURITY_ERR';
+                            break;
+                          case FileError.INVALID_MODIFICATION_ERR:
+                            msg = 'INVALID_MODIFICATION_ERR';
+                            break;
+                          case FileError.INVALID_STATE_ERR:
+                            msg = 'INVALID_STATE_ERR';
+                            break;
+                          default:
+                            msg = 'Unknown Error';
+                            break;
+                        };
+
+                        console.log('Error: ' + msg);
+                        alert('Error: ' + msg);
+
+                      },
+                      _addedSubmitted:function(formData){
+                        
+                    this.formData = formData;
+                         this.Product.save(formData, lang.hitch(this,"_addedSuccessfully"));
+                      },
+                      _saveFile:function(formData,result,grantedBytes){
+                                                var errorHandler = this._fileErrorHandler;
+                        window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+
+                                      window.requestFileSystem(window.PERSISTENT, grantedBytes, function(fs) {
+                                      // Duplicate each file the user selected to the app's fs.
+
+                                        // Capture current iteration's file in local scope for the getFile() callback.
+                                        (function(f) {
+                                          var fileName = result.insertId+'_'+f.name
+                                          fs.root.getFile(fileName, {create: true, exclusive: true}, function(fileEntry) {
+                                            fileEntry.createWriter(function(fileWriter) {
+                                              fileWriter.write(f); // Note: write() can take a File or Blob object.
+                                                console.log(fileName);
+                                            }, errorHandler);
+                                          }, errorHandler);
+                                        })(formData['Product']['image']);
+
+                                      
+                                    }, errorHandler);
+                      },
+                      _addedSuccessfully:function(status,result){
+                        if(!status)
+                        alert(this.Product.validationErrors[0]);
+                      else{
+                        alert('yes');
+                        this.formData['Product']['id']=result.insertId;
+
+                        var formData = this.formData;
+                        var self = this;
+
+                        if(this.formData['Product']['image'] instanceof File){
+                                    //*************************FILE saving**************//
+                                    window.webkitStorageInfo.requestQuota(PERSISTENT, 100*1024*1024, function(grantedBytes) {
+                                          self._saveFile(formData,result,grantedBytes);
+                                          }, function(e) {
+                                            console.log('Error', e);
+                                          });
+                                    
+                                    //**************************************************//
+                        }
+
+                
+                                    
                       }
+                    }
 
                   });
 });
--- js/rijit/helpers/FormHelper.js
+++ js/rijit/helpers/FormHelper.js
@@ -1,6 +1,8 @@
 define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin",
-  "dijit/form/ValidationTextBox","dijit/form/Button","dojo/dom-construct","dojo/_base/lang","dojo/_base/array"],
-          function(declare, WidgetBase, TemplatedMixin, WidgetsInTemplateMixin,ValidationTextBox,Button,domConstruct,lang,array){
+  "dijit/form/ValidationTextBox","dijit/form/Button","dojo/dom-construct","dojo/_base/lang","dojo/_base/array","dojo/on",
+  "dojo/query","dojo/dom-attr"],
+          function(declare, WidgetBase, TemplatedMixin, WidgetsInTemplateMixin,ValidationTextBox,Button,domConstruct,
+            lang,array,on,query,domAttr){
   
                   return declare("rijit.helpers.FormHelper", null, {
   
@@ -10,8 +12,47 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
                       modelName:null,
                       fields:null,
                       createForm: function(arg){
+
+var errorHandler = function(e){
+    console.log('Error', e);
+
+};
+
+    window.webkitStorageInfo.requestQuota(PERSISTENT, 100*1024*1024, function(grantedBytes) {
+                                               window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+
+                                      window.requestFileSystem(window.PERSISTENT, grantedBytes, function(fs) {
+                                      // Duplicate each file the user selected to the app's fs.
+
+                                       
+                                         fs.root.getFile('18_3HD7.jpeg', {}, function(fileEntry) {
+
+    // Get a File object representing the file,
+    // then use FileReader to read its contents.
+    fileEntry.file(function(file) {
+       var reader = new FileReader();
+
+       reader.onloadend = function(theFile) {
+      document.getElementById('debug').innerHTML = ['<img class="thumb" src="', theFile.target.result,
+                            '" title="', "Image", '"/>'].join('');
+       };
+
+       reader.readAsDataURL(file);
+    }, errorHandler);
+
+  }, errorHandler);
+
+
+                                      
+                                    }, errorHandler);
+
+                                          }, function(e) {
+                                            console.log('Error', e);
+                                          });
+
                         this.modelName = arg;
                         this.fields = new Array();
+
                          this.model = eval("new " + arg+"();");
                         this.domNode = domConstruct.create("div",{class:"form "+arg});
                         return this.domNode;
@@ -24,13 +65,47 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
                           options.id=field;
                         }
                         var divField = domConstruct.create("div", {class:"form-row-file"}, this.domNode,"last");
-                        var inputField = domConstruct.create("input", {class:"file",type:"file"}, divField,"last");
+                        var inputField = domConstruct.create("input", {id:field,class:"file",type:"file"}, divField,"last");
+                        
 
                         var labelField = domConstruct.create("div", {class:"title"}, divField,"last");
 
-                        labelField.innerHTML=options.title?options.title:"Drop files here";
+                        labelField.innerHTML='<p>'+(options.title?options.title:"Drop files here")+'</p>';
 
+
+                          
+                        on(inputField, "change", lang.hitch(this, "_handleFileSelect"));
+    
                       },
+                      _handleFileSelect: function(evt){
+                         var files = evt.target.files; // FileList object
+
+if(files[0] && files[0].type.match('image.*')){
+var reader = new FileReader();
+var self = this;
+      // Closure to capture the file information.
+      reader.onload = function(theFile) {
+          // Render thumbnail.
+
+                      
+         var n1 = query('.title',evt.target.parent)
+      n1[0].innerHTML = ['<img class="thumb" src="', theFile.target.result,
+                            '" title="', "Image", '"/>'].join('');
+      };
+
+                document.getElementById('debug').innerHTML=files[0].name;
+
+
+      // Read in the image file as a data URL.
+     
+      reader.readAsDataURL(files[0]);
+       files[0].id=domAttr.get(evt.target,"id");
+self.fields.push(files[0]);
+}
+         },
+
+
+
                       input: function(field,options){
                         if(options && options.type==="file"){
                           this._inputFile(field,options);
@@ -67,7 +142,12 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
                         var modelobject = new Array();
                         modelobject[modelName]=new Array();
                         array.forEach(this.fields, function(entry, i){
+                          if(entry instanceof File){
+                            modelobject[modelName][entry.id] = entry;
+                          }
+                          else{
                           modelobject[modelName][entry.get("id")]=entry.get("value");
+                          }
                         });
                         return modelobject;
                       },
@@ -84,8 +164,7 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
                       submit:function(callBack){
                       
                        var formData = this._getData();
-                       var model = this.model;
-                         this.model.save(formData, callBack);
+                       callBack(formData);
                       },
                       _saveCallBack:function(status,result){
                         
